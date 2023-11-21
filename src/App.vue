0669a6e89a263d2753a546221d7261e2d4a883b7
aufe1995 <aufe1995@gmail.com>
2023-11-21 21:18:49 +0900

    add header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,33 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div id="app">
+    <Header></Header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
 
-  <main>
-    <TheWelcome />
-  </main>
+    <Footer></Footer>
+  </div>
 </template>
 
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/common/Header.vue";
+import Footer from "./components/common/Footer.vue";
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+html,
+body {
+  padding: 0;
+  margin: 0;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
 }
 </style>
