a8252ae4e850c4d53b90e1a973fbb2146985a8f3
gold <aufe1995@gmail.com>
2023-12-01 22:21:44 +0900

    change header

--- src/common/Header.vue
+++ src/common/Header.vue
@@ -1,66 +1,54 @@
 <template>
   <header>
     <div class="menuContainer">
-      <div>
+      <div class="logo">
         <router-link to="/">
-          <img alt="Vue logo" src="../assets/logo.png" height="100" />
+          <img alt="logo" src="../assets/logo.png" height="80" />
         </router-link>
       </div>
       <div class="itemContainer">
-        <ul class="item">
-          <li class="item_menu1 item_name">
+        <div class="itemName">
+          <router-link class="itemLink" to="/menu1">메뉴1</router-link>
+          <div class="itemContents">
             <router-link to="/menu1">메뉴1</router-link>
-            <div class="item_menu1_contents item_contents">
-              <div class="contents_menu">
-                <router-link to="/menu1">메뉴1</router-link>
-                <router-link to="/menu1">메뉴1</router-link>
-                <router-link to="/menu1">메뉴1</router-link>
-              </div>
-            </div>
-          </li>
+            <router-link to="/menu1">메뉴1</router-link>
+            <router-link to="/menu1">메뉴1</router-link>
+          </div>
+        </div>
 
-          <li class="item_menu2 item_name">
+        <div class="itemName">
+          <router-link class="itemLink" to="/menu2">메뉴2</router-link>
+          <div class="itemContents">
             <router-link to="/menu2">메뉴2</router-link>
-            <div class="item_menu2_contents item_contents">
-              <div class="contents_menu">
-                <router-link to="/menu2">메뉴2</router-link>
-                <router-link to="/menu2">메뉴2</router-link>
-                <router-link to="/menu2">메뉴2</router-link>
-              </div>
-            </div>
-          </li>
+            <router-link to="/menu2">메뉴2</router-link>
+            <router-link to="/menu2">메뉴2</router-link>
+          </div>
+        </div>
 
-          <li class="item_menu3 item_name">
+        <div class="itemName">
+          <router-link class="itemLink" to="/menu3">메뉴3</router-link>
+          <div class="itemContents">
             <router-link to="/menu3">메뉴3</router-link>
-            <div class="item_menu3_contents item_contents">
-              <div class="contents_menu">
-                <router-link to="/menu3">메뉴3</router-link>
-                <router-link to="/menu3">메뉴3</router-link>
-                <router-link to="/menu3">메뉴3</router-link>
-              </div>
-            </div>
-          </li>
+            <router-link to="/menu3">메뉴3</router-link>
+            <router-link to="/menu3">메뉴3</router-link>
+          </div>
+        </div>
 
-          <li class="item_menu4 item_name">
+        <div class="itemName">
+          <router-link class="itemLink" to="/menu4">메뉴4</router-link>
+          <div class="itemContents">
             <router-link to="/menu4">메뉴4</router-link>
-            <div class="item_menu4_contents item_contents">
-              <div class="contents_menu">
-                <router-link to="/menu4">메뉴4</router-link>
-                <router-link to="/menu4">메뉴4</router-link>
-                <router-link to="/menu4">메뉴4</router-link>
-              </div>
-            </div>
-          </li>
+            <router-link to="/menu4">메뉴4</router-link>
+            <router-link to="/menu4">메뉴4</router-link>
+          </div>
+        </div>
 
-          <li class="item_menu5 item_name">
-            <router-link to="/main">메뉴</router-link>
-            <div class="item_menu5_contents item_contents">
-              <div class="contents_menu">
-                <router-link to="/main">메인</router-link>
-              </div>
-            </div>
-          </li>
-        </ul>
+        <div class="itemName">
+          <router-link class="itemLink" to="/main">메뉴</router-link>
+          <div class="itemContents">
+            <router-link to="/main">메인</router-link>
+          </div>
+        </div>
       </div>
     </div>
   </header>
@@ -71,102 +59,64 @@ export default {};
 </script>
 
 <style>
-
-header a {
-  text-decoration: none;
-}
-
-header .menuContainer {
-  background-color: #c69265;
-  position: absolute;
+.menuContainer {
+  position: fixed;
   top: 0;
   left: 0;
-  z-index: 1;
-  display: flex;
   width: 100%;
-  border-bottom: 1px solid #35495e;
-  justify-content: space-between;
-}
-
-header .itemContainer .item {
+  box-sizing: border-box;
+  padding: 0 20px;
   display: flex;
-  list-style-type: none;
-  margin: 25px 10px 0px 10px;
+  justify-content: space-between;
+  align-items: stretch;
+  border-bottom: 1px solid #2c2a29;
+  font-size: 0;
+  background: #c69265;
 }
 
-header .menuContainer .item .item_name {
+.menuContainer .itemContainer {
+  display: flex;
+}
+.menuContainer .itemContainer .itemName {
   height: 100%;
-  padding: 20px 20px 35px 20px;
-  font-size: 13px;
+  display: flex;
+  align-items: center;
+  padding: 0 0 0 20px;
 }
-
-header .menuContainer .item .item_name a {
+.menuContainer .itemContainer .itemName .itemLink {
+  font-size: 14px;
+  text-decoration: none;
+  font-weight: bold;
+  transition: all 0.3s;
+  color: #000;
+}
+.menuContainer .itemContainer .itemName .itemLink:hover {
   color: #2c2a29;
 }
 
-header .menuContainer .item .item_name:hover a {
-  color: #fff;
-}
-
-header .menuContainer .item .item_menu1:hover {
-  background-color: #2c2a29;
-  border-radius: 6px 6px 0 0;
-}
-
-header .menuContainer .item .item_menu1:hover .item_menu1_contents {
-  display: block; /*hover하면 보이게*/
-  background-color: #2c2a29;
-}
-
-header .menuContainer .item .item_menu2:hover {
-  background-color: #2c2a29;
-  border-radius: 6px 6px 0 0;
-}
-
-header .menuContainer .item .item_menu2:hover .item_menu2_contents {
-  display: block; /*hover하면 보이게*/
-  background-color: #2c2a29;
-}
-
-header .menuContainer .item .item_menu3:hover {
-  background-color: #2c2a29;
-  border-radius: 6px 6px 0 0;
-}
-
-header .menuContainer .item .item_menu3:hover .item_menu3_contents {
-  display: block; /*hover하면 보이게*/
-  background-color: #2c2a29;
-}
-
-header .menuContainer .item .item_menu4:hover {
-  background-color: #2c2a29;
-  border-radius: 6px 6px 0 0;
-}
-
-header .menuContainer .item .item_menu4:hover .item_menu4_contents {
-  display: block; /*hover하면 보이게*/
-  background-color: #2c2a29;
-}
-
-header .menuContainer .item .item_menu5:hover {
-  background-color: #2c2a29;
-  border-radius: 6px 6px 0 0;
-}
-
-header .menuContainer .item .item_menu5:hover .item_menu5_contents {
-  display: block; /*hover하면 보이게*/
-  background-color: #2c2a29;
-}
-
-header .menuContainer .item .item_contents {
+.menuContainer .itemContainer .itemName .itemContents {
+  position: absolute;
+  top: 80px;
+  right: 0;
   width: 100%;
-  position: fixed;
-  margin-top: 22px;
-  left: 0;
-  display: none; /*평소에는 보이지 않게*/
+  box-sizing: border-box;
+  text-align: right;
+  padding: 20px;
+  background: #2c2a29;
+  transition: all 0.3s;
+  opacity: 0;
+  visibility: hidden;
 }
-
-header .menuContainer .item .item_contents .contents_menu {
-  padding: 20px 0;
+.menuContainer .itemContainer .itemName:hover .itemContents {
+  opacity: 1;
+  visibility: visible;
+}
+.menuContainer .itemContainer .itemName .itemContents a {
+  font-size: 14px;
+  text-decoration: none;
+  font-weight: bold;
+  display: inline-block;
+  padding: 0 0 0 20px;
+  color: #fff;
 }
 </style>
